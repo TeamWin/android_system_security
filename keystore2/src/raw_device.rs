134d383ad32843fdad96c2c41e03b2d090853460
bigbiff <bigbiff@teamw.in>
2022-03-31 18:06:31 -0400

    keystore2: don't update the sqlite db in twrp
    Change-Id: I2856e3cd1f7936e508b8a7fd38ef6a0599365392

--- keystore2/src/raw_device.rs
+++ keystore2/src/raw_device.rs
@@ -18,7 +18,7 @@ use crate::{
     database::{
         BlobInfo, BlobMetaData, BlobMetaEntry, CertificateInfo, DateTime, KeyEntry,
         KeyEntryLoadBits, KeyIdGuard, KeyMetaData, KeyMetaEntry, KeyType, KeystoreDB,
-        SubComponentType, Uuid,
+        Uuid,
     },
     error::{map_km_error, Error, ErrorCode},
     globals::get_keymint_device,
@@ -255,8 +255,8 @@ impl KeyMintDevice {
     /// write the upgraded key to the database.
     fn upgrade_keyblob_if_required_with<'a, T, F>(
         &self,
-        db: &mut KeystoreDB,
-        key_id_guard: &KeyIdGuard,
+        _db: &mut KeystoreDB,
+        _key_id_guard: &KeyIdGuard,
         key_blob: KeyBlob<'a>,
         f: F,
     ) -> Result<(T, KeyBlob<'a>)>
@@ -269,10 +269,11 @@ impl KeyMintDevice {
             &key_blob,
             &[],
             f,
-            |upgraded_blob| {
+            |_upgraded_blob| {
                 let mut new_blob_metadata = BlobMetaData::new();
                 new_blob_metadata.add(BlobMetaEntry::KmUuid(self.km_uuid));
 
+                /*
                 db.set_blob(
                     key_id_guard,
                     SubComponentType::KEY_BLOB,
@@ -280,6 +281,8 @@ impl KeyMintDevice {
                     Some(&new_blob_metadata),
                 )
                 .context(ks_err!("Failed to insert upgraded blob into the database"))?;
+
+*/
                 Ok(())
             },
         )?;
